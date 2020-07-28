888706299b8347bfae68ce0a147cf6371dea3ae5
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2020-07-27 21:16:10 -0400

    pdf framework of resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,1 +1,39 @@
 
+import React, { useState, useEffect, useRef } from 'react';
+import { Typography } from '@material-ui/core';
+import "./layout.css"
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+i
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      height: '600px',
+
+    },
+  }));
+
+  
+const Resume= (props) => {
+    const classes = useStyles();
+
+   
+    return (
+    <div className={classes.root}>
+     Resume
+     <ReactPDF
+      file={{
+        url: 'TODO'
+      }}
+    />
+    </div>
+
+    );
+}
+
+export default About
