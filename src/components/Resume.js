bd80259a7d93700739548f51a52a38acad3dc872
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2020-08-12 22:08:48 -0400

    add resume button

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,12 +6,16 @@ import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import { Document, Page } from 'react-pdf';
+import Button from '@material-ui/core/Button';
 
 import file from './resume.pdf'
 
 const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
+      '& > *': {
+        margin: theme.spacing(1),
+      }
     },
 
     
@@ -32,7 +36,9 @@ const Resume= (props) => {
 
         <Typography variant="h5" gutterBottom align ="center"   >
            <u>Resume</u> 
-        </Typography>      
+        </Typography>
+        <Button variant="outlined"><a href="https://skbt27i.github.io/resume.pdf" target = "_blank" >Download Resume</a></Button>
+      
             <Document
         file={file}
         
@@ -40,6 +46,7 @@ const Resume= (props) => {
         <Page pageNumber={1}
         size="A4" style={{ backgroundColor: 'tomato' }} />
       </Document>
+
 
   
     </div>
