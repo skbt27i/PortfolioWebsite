20de0fbae60294267a61add41bb050dfe0ed8394
Santosh <stirumala@ufl.edu>
2021-05-05 18:52:52 -0700

    particle success

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,36 @@ import SEO from "../components/seo"
 import Particles from 'react-particles-js';
 
 const IndexPage = () => (
-  <div id="begin">
-  <div style={{ position: 'absolute'}}>
-        <Particles height="100vh" width="100vw"  />
-  </div>
+  <div id="begin" style={{ position: 'relative', overflow: "hidden" }}>
+
 
   <Layout>
     <SEO title="Home" />
     <h1>
-      <font face = "Jost">Under Construction</font>
+      <font face = "Jost">Under Construction -       Not friendly to all resolutions/mobile yet
+</font>
         </h1>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`}}>
-      <Image />
-     <h1>
-      Not friendly to all resolutions/mobile yet
-      </h1>
-    </div>
+        <Particles
+  params={{
+    particles: {
+      color: {
+        value: "#BD10E0"
+      },
+      line_linked: {
+        color: {
+          value: "#000000"
+        }
+      },
+      number: {
+        value: 50
+      },
+      size: {
+        value: 5
+      }
+    }
+  }}
+/>
+   
   </Layout>
 
   </div>
