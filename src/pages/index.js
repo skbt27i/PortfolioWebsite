c04e0da075a03e5d7cbbe9b3b083b5a6ebefacf7
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2022-03-29 16:38:31 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,43 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import Particles from 'react-particles-js';
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+import Particles from "react-particles-js";
 
 const IndexPage = () => (
-  <div id="begin" style={{ position: 'relative', overflow: "hidden" }}>
-
-
-  <Layout>
-    <SEO title="Home" />
-    <br />
-    <h1>
-      <font face = "Jost">Santosh Tirumala</font>
-    </h1>
-        <Particles
-  params={{
-    particles: {
-      color: {
-        value: "#BD10E0"
-      },
-      line_linked: {
-        color: {
-          value: "#000000"
-        }
-      },
-      number: {
-        value: 50
-      },
-      size: {
-        value: 5
-      }
-    }
-  }}
-/>
-
-  </Layout>
+  <div id="begin" style={{ position: "relative", overflow: "hidden" }}>
+    <Layout>
+      <SEO title="Home" />
+      <br />
+      <div style={{ marginTop: "15%" }}>
+        <h1>
+          <font face="Jost">Santosh Tirumala</font>
+        </h1>
+      </div>
+      <Particles
+        params={{
+          particles: {
+            color: {
+              value: "#BD10E0",
+            },
+            line_linked: {
+              color: {
+                value: "#000000",
+              },
+            },
+            number: {
+              value: 50,
+            },
+            size: {
+              value: 5,
+            },
+          },
+        }}
+      />
+    </Layout>
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;
