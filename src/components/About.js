447e995345a6273198ad860976a80233c03970c3
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2022-03-29 16:39:56 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,132 +1,117 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Typography } from '@material-ui/core';
-import "./layout.css"
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import java from './java.png';
-import js from './js.png';
-import cee from './c.png';
-import py from './py.png';
-import ogl from './ogl.png';
-import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
-import Divider from '@material-ui/core/Divider';
+import React, { useState, useEffect, useRef } from "react";
+import { Typography } from "@material-ui/core";
+import "./layout.css";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import java from "./java.png";
+import js from "./js.png";
+import cee from "./c.png";
+import py from "./py.png";
+import ogl from "./ogl.png";
+import {
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import Divider from "@material-ui/core/Divider";
 
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(1),
-      height: '600px',
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    height: "600px",
+  },
+}));
 
-    },
-  }));
+const About = (props) => {
+  const classes = useStyles();
 
-  
-const About= (props) => {
-
-    const classes = useStyles();
-
-   
-    return (
+  return (
     <div className={classes.root}>
-                  <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <Grid
+          container
+          spacing={0}
+          direction="row"
+          justify="space-between"
+          alignItems="baseline"
+        >
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>
+              <Typography variant="h5" gutterBottom align="center">
+                <u>About</u>
+              </Typography>
 
-    <Grid container spacing={0} 
-  container
-  direction="row"
-  justify="space-between"
-  alignItems="baseline">
-
-        <Grid item xs={3}>
-          <Paper className = {classes.paper}>
-          <Typography variant="h5" gutterBottom align ="center">
-           <u>About</u> 
-          </Typography>
-
-            <Typography  gutterBottom align ="cen" marginRight = "800px" noWrap>
-                <p> Hi! My name is Santosh Tirumala.  </p>
-                <h7><u>Interests</u> </h7>
-                <br/>
+              <Typography gutterBottom align="cen" marginRight="800px">
+                <p> Hi! My name is Santosh Tirumala. </p>
+                <h7>
+                  <u>Interests</u>{" "}
+                </h7>
+                <br />
                 <h8> • Computer graphics </h8>
                 <br />
                 <h8> • Open Source </h8>
                 <br />
                 <h8> • Tennis </h8>
-
-            </Typography>
+              </Typography>
             </Paper>
+          </Grid>
+
+          <Divider orientation="vertical" flexItem />
+
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>
+              <Typography variant="h5" gutterBottom align="center" >
+                <u>Skills &amp; Technologies</u>
+              </Typography>
+              <ul>
+                <li>
+                  <img src={java} width="22" height="22" />
+                  Java
+                </li>
+                <li>
+                  <img src={cee} width="22" height="22" />
+                  C++
+                </li>
+                <li>
+                  <img src={js} width="22" height="22" />
+                  JavaScript
+                </li>
+                <li>
+                  <img src={py} width="22" height="22" />
+                  Python
+                </li>
+                <li>
+                  <img src={ogl} width="40" height="22" />
+                  OpenGL
+                </li>
+              </ul>
+            </Paper>
+          </Grid>
+
+          <Divider orientation="vertical" flexItem />
+
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>
+              <Typography variant="h5" gutterBottom align="center" >
+                <u>Experience</u>
+              </Typography>
+              <ul>
+                <li>Amazon SDE Intern</li>
+                <li>UF University Scholar</li>
+              </ul>
+            </Paper>
+          </Grid>
         </Grid>
+      </ThemeProvider>
+    </div>
+  );
+};
 
-        <Divider orientation="vertical" flexItem />
-
-        <Grid item xs={3}>
-
-        <Paper className = {classes.paper}>
-
-          <Typography variant="h5" gutterBottom align ="center" noWrap>
-          <u>Skills &amp; Technologies</u>  
-            </Typography>
-            <ul>
-
-        <li>
-       <img src={java}  width="22" height="22" />
-        Java
-    </li>
-    <li>
-       <img src={cee} width="22" height="22"/>
-       C++
-    </li>
-    <li>
-       <img src={js} width="22" height="22"/>
-        JavaScript
-    </li>
-    <li>
-            <img src={py} width="22" height="22"/>
-           Python
-    </li>
-    <li>
-        <img src ={ogl} width="40" height="22"/>
-
-        OpenGL
-    </li>
-    </ul>
-           </Paper>
-        </Grid>
-        <Divider orientation="vertical" flexItem />
-
-        <Grid item xs={3}>
-        <Paper className = {classes.paper}>
-
-          <Typography variant="h5" gutterBottom align="center" noWrap>
-          <u>Experience</u>  
-        </Typography>
-        <ul>
-
-    <li>
-    Amazon SDE Intern
-</li>
-<li>
-    UF University Scholar
-</li>
-
-       </ul>
-       </Paper>
-        </Grid>
-    </Grid>
-
-            
-            
-            
-    </ThemeProvider>
-          
-            </div>
-
-    );
-}
-
-export default About
+export default About;
