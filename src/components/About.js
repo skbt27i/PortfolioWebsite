f762a5242dc6ec4f33c3a6615ed3a169c9cc24b6
Santosh <stirumala@ufl.edu>
2024-09-08 20:38:25 -0700

    refactoring and change design slightly

--- src/components/About.js
+++ src/components/About.js
@@ -1,129 +1,120 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { Typography } from "@material-ui/core";
 import "./layout.css";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import java from "./java.png";
-import js from "./js.png";
-import cee from "./c.png";
-import py from "./py.png";
-import ogl from "./ogl.png";
-import {
-  createMuiTheme,
-  responsiveFontSizes,
-  ThemeProvider,
-} from "@material-ui/core/styles";
+import java from "../images/java.png";
+import js from "../images/js.png";
+import cee from "../images/c.png";
+import py from "../images/py.png";
+import ogl from "../images/ogl.png";
+import { createMuiTheme, responsiveFontSizes, ThemeProvider } from "@material-ui/core/styles";
 import Divider from "@material-ui/core/Divider";
 
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
 
-
 const useStyles = makeStyles((theme) => ({
   root: {
+    display: 'flex',
+    justifyContent: 'center', 
+    alignItems: 'center',   
     flexGrow: 1,
+    minHeight: '100vh', 
+    flexDirection: 'column',
+    width: '100%', 
+    margin: 0,
+    padding: 0,
+  },
+  container: {
+    flexGrow: 1,
+    width: '100%',
   },
   paper: {
-    padding: theme.spacing(1),
-    height: "100%"
+    padding: theme.spacing(2),
+    textAlign: "center",
+    height: '100%',
   },
-  stretch: { height: "100%" },
-
+  image: {
+    width: 24,
+    height: 24,
+    verticalAlign: "middle",
+    marginRight: theme.spacing(1),
+  },
+  divider: {
+    margin: theme.spacing(2, 0),
+  }
 }));
 
-const About = (props) => {
+const About = () => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <ThemeProvider theme={theme}>
-        <Grid
-          container
-          direction="row"
-          justify="space-between"
-          alignItems="stretch"
-          spacing={0}
-        >
-          <Grid item xs={3} sm={4} md={4}>
+        <Grid container direction="column" spacing={3} className={classes.container}>
+          <Grid item xs={12}>
             <Paper className={classes.paper}>
-              <Typography variant="h5" gutterBottom align="center">
+              <Typography variant="h5" gutterBottom>
                 <u>About</u>
               </Typography>
-
-              <Typography variant="h8">
-                <p> Hi! My name is Santosh Tirumala. </p>
-                <h7>
-                  <u>Interests</u>{" "}
-                </h7>
+              <Typography variant="body1">
+                Hi! My name is Santosh Tirumala.
+                <br />
+                <br />
+                <u>Interests</u>
                 <div>
+                  Computer graphics
                   <br />
-                  Computer graphics 
+                  Open Source
                   <br />
-                  <br />
-                  Open Source 
-                  <br />
-                  <br />
-                   Tennis 
+                  Tennis
                 </div>
               </Typography>
             </Paper>
           </Grid>
 
-          <Divider orientation="vertical" flexItem />
+          <Divider className={classes.divider} />
 
-          <Grid item xs={4} sm={4} md={4}>
+          <Grid item xs={12}>
             <Paper className={classes.paper}>
-              <Typography variant="h5" gutterBottom align="center">
+              <Typography variant="h5" gutterBottom>
                 <u>Skills</u>
               </Typography>
-              <Typography gutterBottom align="center">
-                  <h8>
-                    <img src={java} width="22" height="22" />
-                    Java
-                  </h8>
+              <Typography variant="body1">
+                <div>
+                  <img src={java} className={classes.image} alt="Java" />
+                  Java
                   <br />
+                  <img src={cee} className={classes.image} alt="C++" />
+                  C++
                   <br />
-                  <h8>
-                    <img src={cee} width="22" height="22" />
-                    C++
-                  </h8>
+                  <img src={js} className={classes.image} alt="JavaScript" />
+                  JavaScript
                   <br />
+                  <img src={py} className={classes.image} alt="Python" />
+                  Python
                   <br />
-                  <h8>
-                    <img src={js} width="22" height="22" />
-                    JavaScript
-                  </h8>
-                  <br />
-                  <br />
-                  <h8>
-                    <img src={py} width="22" height="22" />
-                    Python
-                  </h8>
-                  <br />
-                  <br />
-                  <h8>
-                    <img src={ogl} width="40" height="22" />
-                    OpenGL
-                  </h8>
+                  <img src={ogl} className={classes.image} alt="OpenGL" />
+                  OpenGL
+                </div>
               </Typography>
             </Paper>
           </Grid>
 
-          <Divider orientation="vertical" flexItem />
+          <Divider className={classes.divider} />
 
-          <Grid item xs={4} sm={3} md={3}>
+          <Grid item xs={12}>
             <Paper className={classes.paper}>
-              <Typography variant="h6" gutterBottom align="center">
-              <u>Experience</u>
+              <Typography variant="h6" gutterBottom>
+                <u>Experience</u>
               </Typography>
-
-              <Typography gutterBottom align="center">
-                  Amazon SDE1, Alexa Speech
-                  <br />
-                  <br />
-                  UF University Scholar
-                </Typography>
+              <Typography variant="body1">
+                Amazon SDE1, Alexa Speech
+                <br />
+                UF University Scholar
+              </Typography>
             </Paper>
           </Grid>
         </Grid>
